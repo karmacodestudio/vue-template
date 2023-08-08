d1864de5f31b4c60bc753cc8327266840665581e
Marta Coscujuela <karmacodestudio@gmail.com>
2023-08-08 11:06:00 +0200

    initial-commit

---
+++ src/Main.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Hola1</div>
+  <div>Hola2</div>
+</template>
---
+++ src/Services/ServiceTest.ts
@@ -0,0 +1,8 @@
+class ServiceTest {
+  hola = "hola";
+  constructor(hola: string) {
+    this.hola = hola;
+  }
+}
+
+export default new ServiceTest("mundo");
